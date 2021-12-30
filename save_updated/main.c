5ecca6589ad05b01639dc8e3246f966d2cc2a75d
M-Amr982 <96430851+M-Amr982@users.noreply.github.com>
2021-12-30 16:05:08 +0200

    Add files via upload

---
+++ save_updated/main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include "movesManager.h"
+#include "RandomLineGen.h"
+#include "SAVEGAME.h"
+#include "GameCore.h"
+#include "gameloop.h"
+
+
+
+//leave gameLoop as the last method before main --->ok
+//game loop for single player mode
+
+
+
+int main(){
+
+    //HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    //SetConsoleTextAttribute(handle, FOREGROUND_RED);
+
+    srand(time(0));
+    int gridDim;
+    int modeNum ;
+    printf("Select mode\n[1]single player\n[2]two players\n[3]load game\n");
+    scanf("%d",&modeNum);
+    // printf("Enter dimension(nxn): ");
+    // scanf("%d", &gridDim);
+    // //clearing buffer
+    // getchar();
+    // int arrayDim = gridDim*2-1;
+    // char gameArray[arrayDim][arrayDim];
+    // initGameArray(arrayDim, gameArray);
+    //printGrid(arrayDim, gameArray);
+
+    //number of lines = 2*gridDim*(gridDim-1)
+    //stores coordinates of last line
+    //resets in at the beginning of gameLoop()
+    
+    
+    if(modeNum != 3){
+
+        //new game
+        printf("Enter dimension(nxn): ");
+        scanf("%d", &gridDim);
+        //clearing buffer
+        getchar();
+        //initializing
+        int arrayDim = gridDim*2-1;
+        char gameArray[arrayDim][arrayDim];
+        initGameArray(arrayDim, gameArray);
+        int movesArray[2*gridDim*(gridDim-1)][MOVES_ARRAY_WIDTH];
+        resetMovesArray(gridDim, movesArray);
+        
+        printGrid(arrayDim, gameArray);
+        
+        if(modeNum == 1){
+            gameLoopVScom(gridDim, gameArray, movesArray);
+        }else if(modeNum == 2){
+            gameLoop(gridDim,gameArray,movesArray);
+        }
+    }else if(modeNum == 3){
+        printf("loading...\n");
+        FILE* filepointer;
+        filepointer = fopen("data.bin","rb");
+        
+        if(filepointer==NULL){
+            printf("error reading file\n");
+            exit(1);
+        }
+        fread(&savedgame,sizeof(struct gamedata),1,filepointer);
+        
+        printf("done loading\n");
+
+        //loading dims
+        int gridDim=savedgame.Dimensions;
+        int arrayDim = gridDim*2-1;
+        char gameArray[arrayDim][arrayDim];
+        initGameArray(arrayDim, gameArray);
+        int movesArray[2*gridDim*(gridDim-1)][MOVES_ARRAY_WIDTH];
+        resetMovesArray(gridDim, movesArray);
+
+        //loading moves
+        for(int i=0;i<(2*gridDim*(gridDim-1));i++){
+            for(int j=0;j<MOVES_ARRAY_WIDTH;j++){
+                movesArray[i][j]=savedgame.moves[i][j];
+            }
+        }
+
+        //loading gameArray
+        for(int i=0;i<arrayDim;i++){
+            for(int j=0;j<arrayDim;j++){
+                gameArray[i][j]=savedgame.Grid[i][j];
+            }
+        }
+        printGrid(arrayDim, gameArray);
+        gameLoopVScom(gridDim, gameArray, movesArray);
+    }
+    else
+    printf("not a valid option\n");
+    printf("Press ENTER to continue...");
+
+    while(getchar() != '\n'){
+
+    }
+    return 0;
+}
